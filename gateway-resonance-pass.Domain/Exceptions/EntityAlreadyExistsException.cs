63ac84d203366e51236cab8c449c5e711516f0af
ricardoth <ricardotilleriaochoa@gmail.com>
2024-04-04 16:58:55 -0300

    Avance en arquitectura proyecto: Mediatr, Repository, Onion Architecture

---
+++ gateway-resonance-pass.Domain/Exceptions/EntityAlreadyExistsException.cs
@@ -0,0 +1,23 @@
+﻿namespace gateway_resonance_pass.Domain.Exceptions
+{
+    [ExcludeFromCodeCoverage]
+    public class EntityAlreadyExistsException : Exception
+    {
+        public EntityAlreadyExistsException()
+        {
+        }
+
+        public EntityAlreadyExistsException(string message) : base(message)
+        {
+        }
+
+        public EntityAlreadyExistsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public EntityAlreadyExistsException(string name, object[] keys) : base($"Entity \"{name}\" ({string.Join(",", keys)} already exists")
+        {
+            
+        }
+    }
+}
---
+++ gateway-resonance-pass.Domain/ValueObjects/GatewayConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace gateway_resonance_pass.Domain.ValueObjects
+{
+    public class GatewayConfig
+    {
+        public string SubscriptionKey { get; set; }
+        public bool ValidateIdentification { get; set; }
+        public GatewayConfigItemDecimatio ApiDecimatio { get; set; }
+        public GatewayConfigItemIdentification ApiIdentification { get; set; }
+
+    }
+
+    public class GatewayConfigItemDecimatio
+    {
+        public string UserBasicAuth { get; set;}
+        public string PassBasicAuth { get; set;}
+        public string Url { get; set; } 
+    }
+
+    public class GatewayConfigItemIdentification
+    {
+        public string Token { get; set; }
+        public string Url { get; set; }
+    }
+}
